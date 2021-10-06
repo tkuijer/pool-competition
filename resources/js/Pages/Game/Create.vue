bc35850d9ef29de621b73a2b2b0b0e903327bbe6
Tom Kuijer <t.kuijer@ifresh.nl>
2021-10-06 11:31:12 +0200

    Frontend refactor to use Inertia Form object

--- resources/js/Pages/Game/Create.vue
+++ resources/js/Pages/Game/Create.vue
@@ -1,174 +1,115 @@
 <template>
-  <Head title="foo"/>
+    <Head title="foo" />
 
-  <div v-if="activeStep === 'selectPlayers'">
+    <div v-if="activeStep === 1">
+        <h2 class="font-bold pb-4 text-xl">
+            Wie won er?
+        </h2>
+        <ul class="grid grid-cols-2 gap-5">
+            <li v-for="player in players">
+                <select-player-button @click="form.winner_id = player.id; activeStep++">{{ player.name }}</select-player-button>
+            </li>
+        </ul>
+    </div>
+    <div v-if="activeStep === 2">
+        <h2 class="font-bold pb-4 text-xl">
+            Welke kleur speelde {{ winner.name }}?
+        </h2>
+        <ul class="grid grid-cols-2 gap-x-5">
+            <li v-for="color in colors">
+                <button
+                    class="block text-lg py-3 rounded-lg border border-gray-200 w-full hover:bg-blue-50"
+                    @click="form.winner_color = color; activeStep++"
+                >{{ color }}
+                </button>
+            </li>
+        </ul>
+    </div>
+    <div v-if="activeStep === 3">
+        <h2 class="font-bold pb-4 text-xl">
+            Tegen wie speelde {{ winner.name }}?
+        </h2>
+        <ul class="grid grid-cols-2 gap-5">
+            <li v-for="opponent in opponents">
+                <select-player-button @click="form.opponent_id = opponent.id; activeStep++">{{ opponent.name }}</select-player-button>
+            </li>
+        </ul>
+    </div>
+    <div v-if="activeStep === 4">
+        <h2 class="font-bold pb-4 text-xl">
+            Hoe won {{ winner.name }}?
+        </h2>
+        <ul class="grid grid-cols-3 gap-5">
+            <li v-for="method in winningMethods">
+                <button
+                    class="block text-lg py-3 rounded-lg border border-gray-200 w-full hover:bg-blue-50"
+                    @click="form.win_method = method; activeStep++"
+                >{{ method }}
+                </button>
+            </li>
+        </ul>
+    </div>
+    <div v-if="activeStep === 5">
+        <h2 class="font-bold pb-4 text-xl">Samenvatting:</h2>
+        <span class="bold">Winnaar: </span>{{ winner.name }}<br />
+        Speelde: {{ form.winner_color }} <br />
+        Tegen: {{ opponent.name }}<br />
+        Gewonnen via: {{ form.win_method }}
 
-    <h2 class="font-bold pb-4 text-xl">
-      <template v-if="! challengerIsSet">selecteer speler</template>
-      <template v-if="challengerIsSet">{{ challenger.name }} vs.</template>
-    </h2>
-    <ul class="grid grid-cols-2 gap-5">
-      <li v-for="player in selectablePlayers">
-        <select-player-button @click="selectPlayer(player)">{{ player.name }}</select-player-button>
-      </li>
-    </ul>
-  </div>
-  <div v-if="activeStep === 'selectColor'">
-    Wat speelt {{ challenger.name }}?
-    <ul class="grid grid-cols-2 gap-x-5">
-      <li v-for="color in colors">
-        <button
-            class="block text-lg py-3 rounded-lg border border-gray-200 w-full hover:bg-blue-50"
-            @click="selectColor(color)"
-        >{{ color }}
-        </button>
-      </li>
-    </ul>
-  </div>
-  <div v-if="activeStep === 'selectWinner'">
-    Wie won?
-    <ul class="grid grid-cols-2 gap-x-5">
-      <li v-for="player in activePlayers">
-        <button
-            class="block text-lg py-3 rounded-lg border border-gray-200 w-full hover:bg-blue-50"
-            @click="selectWinner(player)"
-        >{{ player.name }}
-        </button>
-      </li>
-    </ul>
-  </div>
-  <div v-if="activeStep === 'selectWinningMethod'">
-    Hoe won {{ winner.name }}?
-    <ul class="grid grid-cols-3 gap-x-5">
-      <li v-for="method in winningMethods">
-        <button
-            class="block text-lg py-3 rounded-lg border border-gray-200 w-full hover:bg-blue-50"
-            @click="selectWinningMethod(method)"
-        >{{ method }}
-        </button>
-      </li>
-    </ul>
-  </div>
-  <div v-if="activeStep === 'submitData'">
-    <button @click="submitData" class="bg-green-500 text-white block text-lg py-3 rounded-lg w-full">opslaan</button>
-  </div>
+        <button :disabled="form.processing" @click.prevent="form.post(route('game.store'))" class="bg-green-500 text-white block text-lg py-3 rounded-lg w-full">opslaan</button>
+    </div>
 
 
 </template>
 
 <script>
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import {Head, Link} from '@inertiajs/inertia-vue3';
 import Layout from '@/Layouts/Default.vue';
 import SelectPlayerButton from "@/Components/SelectPlayerButton";
+import {useForm} from "@inertiajs/inertia-vue3";
+
+const activeStep = 1;
 
 export default {
-  layout: Layout,
-  components: {
-    SelectPlayerButton,
-    Head,
-    Link,
-  },
-  props: {
-    players: Array,
-    winningMethods: Array,
-    colors: Array
-  },
-  data() {
-    return {
-      challenger: null,
-      challengerColor: null,
-      opponent: null,
-      winner: null,
-      winningMethod: null
-    };
-  },
-  methods: {
-    selectPlayer: function(player) {
-      if (!this.challengerIsSet) {
-        this.challenger = player;
-        return;
-      }
+    layout: Layout,
+    components: {
+        SelectPlayerButton,
+        Head,
+        Link,
+    },
+    props: {
+        players: Array,
+        winningMethods: Array,
+        colors: Array
+    },
+    data() {
+        return {
+            activeStep: activeStep,
+        };
+    },
+    setup() {
+        const form = useForm({
+            winner_id: null,
+            opponent_id: null,
+            winner_color: null,
+            win_method: null,
+        })
 
-      this.opponent = player;
+        return {form}
     },
-    selectColor: function(color) {
-      this.challengerColor = color;
+    computed: {
+        winner: function () {
+            return this.players.filter(player => player.id === this.form.winner_id)[0];
+        },
+        opponent: function() {
+            return this.players.filter(player => player.id === this.form.opponent_id)[0];
+        },
+        opponents: function () {
+            return this.players.filter((player) => {
+                return player.id !== this.form.winner_id;
+            });
+        },
     },
-    selectWinner: function(player) {
-      this.winner = player;
-    },
-    selectWinningMethod: function(method) {
-      this.winningMethod = method;
-    },
-    submitData: function() {
-      this.$inertia.visit(this.route('game.store'), {
-        method: 'post',
-        data: {
-          challenger: this.challenger,
-          opponent: this.opponent,
-          challengerColor: this.challengerColor,
-          winner: this.winner,
-          winningMethod: this.winningMethod
-        }
-      });
-    }
-  },
-  computed: {
-    challengerIsSet: function() {
-      return this.challenger !== null;
-    },
-    opponentIsSet: function() {
-      return this.opponent !== null;
-    },
-    playersAreSelected: function() {
-      return this.challengerIsSet && this.opponentIsSet;
-    },
-    colorIsSelected: function() {
-      return this.challengerColor !== null;
-    },
-    winnerIsSelected: function() {
-      return this.winner !== null;
-    },
-    winningMethodIsSelected: function() {
-      return this.winningMethod !== null;
-    },
-    selectablePlayers: function() {
-      if (!this.challenger) {
-        return this.players;
-      }
-
-      return this.players.filter((player) => {
-        return player !== this.challenger;
-      });
-
-    },
-    activePlayers: function() {
-      return [
-        this.challenger,
-        this.opponent
-      ];
-    },
-    activeStep: function() {
-      if (!this.playersAreSelected) {
-        return 'selectPlayers';
-      }
-
-      if (!this.colorIsSelected) {
-        return 'selectColor';
-      }
-
-      if (!this.winnerIsSelected) {
-        return 'selectWinner';
-      }
-
-      if (!this.winningMethodIsSelected) {
-        return 'selectWinningMethod';
-      }
-
-      return 'submitData';
-
-    }
-  },
 
 };
 </script>
