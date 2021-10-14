d2849d97cfe51035564df51fec62004d1ac481c4
Stephan Bouman <s.bouman@ifresh.nl>
2021-10-14 21:07:16 +0200

    Random balkeuzes in de seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,23 @@ namespace Database\Seeders;
 use App\Models\Game;
 use App\Models\Player;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class DatabaseSeeder extends Seeder
 {
+    public const MAXIMUM_PLAYERS = 2;
+
+    private Collection $players;
+
+    private Collection $colors;
+
+    private bool $winner = true;
+
+    public function __construct()
+    {
+        $this->resetColors();
+    }
+
     /**
      * Seed the application's database.
      *
@@ -17,25 +31,60 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
 
-        // Create 10 random players
-        $players = Player::factory()->count(10)->create();
+        $this->players = Player::factory()->count(10)->create();
 
-        // Create 100 random games
-        $games = Game::factory()->count(100)->create();
+        Game::factory(100)->create()
+            ->each(function (Game $game) {
+                $this->addPlayersToGame($game);
+            });
+    }
 
-        // Attach players to games
-        $games->each(function (Game $game) use ($players) {
-            $colors = ['solid', 'stripe'];
+    public function addPlayersToGame(Game $game)
+    {
+        for ($i = 0; $i < self::MAXIMUM_PLAYERS; $i++) {
+            $this->addPlayerToGame($game);
+        }
 
-            $game->players()->attach($players->random()->id, [
-                'color' => $colors[0],
-                'winner' => 1,
-            ]);
+        $this->resetColors();
+    }
 
-            $game->players()->attach($players->random()->id, [
-                'color' => $colors[1],
-                'winner' => 0,
-            ]);
+    private function getRandomPlayer()
+    {
+        if (! $this->players) {
+            return null;
+        }
+
+        return $this->players->random()->id;
+    }
+
+    private function getRandomColor()
+    {
+        $selectedColor = $this->colors->random();
+
+        $this->colors = $this->colors->reject(function ($availableColor) use ($selectedColor) {
+            return $selectedColor === $availableColor;
         });
+
+        return $selectedColor;
+    }
+
+    private function resetColors()
+    {
+        $this->colors = collect(Game::getColors());
+    }
+
+    private function getWinner()
+    {
+        $this->winner = ! $this->winner;
+
+        return $this->winner;
+    }
+
+    private function addPlayerToGame(Game $game)
+    {
+        $game->players()->attach($this->getRandomPlayer(), [
+            'color' => $this->getRandomColor(),
+            'winner' => $this->getWinner(),
+        ]);
     }
 }
