2a2a79813ee910dd2d4f3f2dc9f7607f2090763b
Tom Kuijer <t.kuijer@ifresh.nl>
2023-05-30 16:06:18 +0200

    Lentemodus is GO

--- resources/js/Layouts/Default.vue
+++ resources/js/Layouts/Default.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-500">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-green-500">
         <div>
             <Link href="/" class="flex p-6 bg-gray-100 shadow rounded-xl mt-10">
                 <Logo class="w-96 h-auto fill-current text-gray-500"/>
             </Link>
         </div>
 
-        <div class="w-full sm:max-w-7xl mt-6 px-6 py-4 bg-gray-200 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-7xl mt-6 px-6 py-4 bg-pink-50 text-gray-800 shadow-lg overflow-hidden sm:rounded-lg mb-10">
             <slot/>
         </div>
     </div>
